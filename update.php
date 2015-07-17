b425df75dd8f399e6e1dbf5e1a9e9791dd973bd4
msg100 <profsilvio@outlook.com>
2015-07-17 11:05:23 -0300

    executa um update

---
+++ update.php
@@ -0,0 +1,49 @@
+<?php
+
+/* * ************************************************
+ * *********** CRIANDO TABELA REDBEAN **************
+ * *************************************************
+ * Por Sílvio Giavaroto - Jul 2015
+ * Fonte ReadBean
+ * ************************************************
+ */
+
+
+// Invocando arquivo de configuração RB.PHP
+require 'rb.php';
+
+// Parâmetros de configuração Base MySQL
+R::setup("mysql:host=localhost;dbname=red", 'root', '');
+
+
+// Criando uma tabela chamada usuários em uma base existente chamada red
+$usuarios = R::dispense('usuarios');
+
+// Criando os campos e populando com dados
+$usuarios->login = 'admin';
+$usuarios->senha = md5(123);  // criando uma senha criptografada com hash MD5
+// acrescentando mais um campo na tabela
+$usuarios->idade = 46;
+// Armazenando resultado
+R::store($usuarios);
+
+// Executando Update trocando login usuário de id 1 , ou seja, admin para jumento
+
+$up = R::load('usuarios', 1);
+$up->login = 'Jumento';
+R::store($up);
+
+
+
+
+
+
+/******************************************************************
+ * ************************* RESULTADO FINAL **********************
+ * +--------------------------------------------------------------+
+ * |id | login  | senha                              |idade       +
+ * +--------------------------------------------------------------+
+ * + 1 | jumento  | 202cb962ac59075b964b07152d234b70 | 46         +
+ * +--------------------------------------------------------------+
+ */
+
